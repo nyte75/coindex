de58bda85b68b75d51daf7d029e587e56db5eaac
Nathan Kima Yuave <nate10yuave@protonmail.com>
2021-06-15 11:24:19 +1100

    adding --rank, not working yet

--- bin/coindex-check.js
+++ bin/coindex-check.js
@@ -4,13 +4,17 @@ const check  = require('../commands/check');
 program
 .command('price')
 .description("Check prices of coins")
-.option('--coin <type>', 
+.option('--coin <type>',
     'Add specific coin types in CSV format', 
     'BTC, ETH, XRP'
 )
 .option('--cur <currency>', 
     "Change the currency", 
     'USD'
+)
+.option('--rank <type>',
+    'Show according to rank',
+    '1, 2, 3'
 )
 .action(cmd => check.price(cmd));
 
--- commands/check.js
+++ commands/check.js
@@ -8,7 +8,7 @@ const check = {
             const key = keyManager.getKey();
 
             const api = new CryptoAPI(key);
-            const priceOutputData = await api.getPriceData(cmd.coin, cmd.cur);
+            const priceOutputData = await api.getPriceData(cmd.coin, cmd.cur, cmd.rank);
             console.log(priceOutputData);
         } catch (error) {
             console.error(error.message.red);
